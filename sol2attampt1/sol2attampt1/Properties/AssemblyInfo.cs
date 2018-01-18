93caba0607fa9113db8821c8ec2f9351c4cfa451
Roman <my-email@email.com>
2018-01-19 01:26:34 +0200

    commit1

---
+++ sol2attampt1/sol2attampt1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("sol2attampt1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Win-Torrent Yagd")]
+[assembly: AssemblyProduct("sol2attampt1")]
+[assembly: AssemblyCopyright("Copyright © Win-Torrent Yagd 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8143c34-55d4-4a8a-8620-e3624793408b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
