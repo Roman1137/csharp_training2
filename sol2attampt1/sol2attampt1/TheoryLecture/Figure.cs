17aa26005bbad1a154f1213afede780923bea9f9
Roman <my-email@email.com>
2018-01-21 17:25:20 +0200

    HomeTask1.6

---
+++ sol2attampt1/sol2attampt1/TheoryLecture/Figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sol2attampt1
+{
+    class Figure
+    {
+        private bool colored = false;
+        public bool Colored
+        {
+            get { return colored; }
+            set { colored = value; }
+        }
+    }
+}
---
+++ sol2attampt1/sol2attampt1/TheoryLecture/Square.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sol2attampt1
+{
+    class Square:Figure
+    {
+        private int size;
+       
+        public Square(int size)
+        {
+            this.size = size;
+        }
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+      
+
+    }
+}
